5c890b681eb584f63023b1c9ecd7b84959ea6a0e
jr1248 <55035698+jr1248@users.noreply.github.com>
2020-10-22 12:46:59 -0400

    Add files via upload

--- LIF_model.py
+++ LIF_model.py
@@ -14,23 +14,39 @@ tau_ref = float(input("How long will the refractory period be:")); #refractory p
 dt = float(input("What is the time step:")); #simulation time step 
 Vt = float(input("Spike Threshold:")); #spike threshold 
 total_simulation = np.arange(0, time+dt,dt); #is an array of time 
-Vm = [0] * len(total_simulation);
+Vm = np.zeros(len(total_simulation));
 dv = float(input("Change in voltage:")); #simmilar to dt but for voltage not time 
+currArr = np.zeros(len(total_simulation));
 
 #Our original equation is given by tau_m(dv/dt) = -Vm(t) + Rm(I(t)) and we want to find Vm
 #Let's use this equation to solve for each Vm at time t
+currentS = float(input("Where does your current start:"))
+stop = float(input("Where does your current end:"));
+
+for j in range(int(currentS / dt), int(stop / dt)):
+    currArr[j] = I
+
 for i in range(len(total_simulation)):
-    if time > tRef:
-        Vm[i] = Vm[i - 1] + (-Vm[i - 1] + I*Rm)/tau_m*dt;
-        if Vm[i] >= dv:
+    if total_simulation[i] > tRef:
+        Vm[i] = Vm[i - 1] + (-Vm[i - 1] + currArr[i-1]*Rm)/tau_m*dt;
+        if Vm[i] >= Vt:
             Vm[i] += dv;
-            tRef = time + tau_m;
+            tRef = total_simulation[i] + tau_ref;
+
+
+
 
 #Plot points 
-np.array(total_simulation);
-np.array(Vm);
-plt.plot(total_simulation,Vm);
-plt.xlabel("Time");
-plt.ylabel("Membrane Potential");
+fig, ax1 = plt.subplots();
+
+ax2 = ax1.twinx();
+ax1.plot(total_simulation,Vm,'g--');
+ax2.plot(total_simulation,currArr,'r-');
+
+ax1.set_xlabel('Time');
+ax1.set_ylabel('Membrane Potential(mV)', color='g');
+ax2.set_ylabel("Current", color='r');
+
 plt.title("LIF MODEL");
-plt.show();
+
+plt.show()
