91bab37446fab55349bd19121808b3548361c21f
jiesparklin <33667998+jiesparklin@users.noreply.github.com>
2020-11-27 04:18:43 -0500

    Update snn_net.py

--- Assignment 2/Jie/snn_net.py
+++ Assignment 2/Jie/snn_net.py
@@ -3,6 +3,7 @@ from snn_neuron import LIFFCLayer
 from snn_optim import HebbianOptim
 from hyperparams import *
 from snn_utils import *
+import matplotlib.pyplot as plot
 
 class SNN_MLP(object):
     def __init__(self, struct, optimizer_cls = HebbianOptim, params = {\
@@ -30,6 +31,7 @@ class SNN_MLP(object):
 #         input()
         T = input_trains.shape[-1]
         outputTrains = np.zeros((self.layers[-1].synapses.shape[-1],T))
+
         for t in range(T):
             vIn = input_trains[:,t]
             vOut = vIn
@@ -37,7 +39,7 @@ class SNN_MLP(object):
                 layer = self.layers[i]
 #                 print(vOut)
 #                 input()
-                vOut, _ = layer.forward_step(vOut)
+                vOut, _ = layer.forward_step(vOut,check=0)
 #                 print(vOut)
 #                 input()
             outputTrains[:,t] = vOut
@@ -49,34 +51,35 @@ class SNN_MLP(object):
         L = len(self.layers)  ## 1个layer
         trained = np.zeros_like(features)
         T = features.shape[-1]
+
+        if len(np.where(features[0]>0)[0]) != len(np.where(features[1]>0)[0]):
+            check = 0
+        else:
+            check = 1
+
+
         for t in range(T):
             # forward pass
             vIn = features[:,t]
             targets = target_train[:,t]
+
+            
             for i in range(L):
                 # input and output spike
-                vOut, potential = self.layers[i].forward_step(vIn)
-#                 print("input: " + str(vIn))
-#                 print("output: " + str(vOut))
-#                 input()
+                vOut, _ = self.layers[i].forward_step(vIn,check)
+
                 # weight updates
                 optimizer = self.optimizers[i]
                 optimizer.record({"pre": vIn, "post": vOut})
-                # input for next layer
-                vIn = vOut
-            # update from the last layer to the first layer
-            #for i in range(L):
-                # the optimizer
-                #optimizer = self.optimizers[L-1-i]
-                # update target train
-#                 print("target_train:" + str(target_train))
-#                 input()
 
-                optimizer.record({"post": targets}, replace = True) ### actual target成了post activity.
+                optimizer.record({"post": targets}, replace = True) ### actual target becomes post activity.
+
                 # optimize
                 optimizer.step()
                 self.layers[L-1-i].synapses = optimizer.W
 
+    
+ 
  
        
     def fit(self, data_loader, params):
@@ -90,35 +93,48 @@ class SNN_MLP(object):
         # training
         error = np.float("inf")
         for epoch in range(epochLimit):
-            #data_loader.get_n_batch(0)
-            for j in range(25):
-                rawData,encodedData = data_loader.get_batch(encoding = "firing_rate")#[4,3,timestep] 4个data,每个3行：1和2是x和ytrain,3是Output
+            rawData,encodedData = data_loader.get_batch(encoding = "firing_rate")
+            for i in range(encodedData.shape[0]):
+                features = encodedData[i,:-1,:]
+                targets = encodedData[i,-1:,:]
+                # outplot = np.where(features[i,:]>0)[0]
+                # outplot = outplot.astype(float)
+                # for q in range(len(outplot)):
+                #     outplot[q] = (outplot[q]+1)/1000
+                # #outplot = outplot.astype(float)
+                # plot.eventplot(outplot,orientation = "horizontal",linelengths = 0.1, lineoffsets = 0.5)
+                # plot.ylim((0,1.0))
+                # plot.xlim((0, 1.0))
+                # if len(outplot)<20:
+                #     plot.title("spike train for input '0' over 1000 ms")
+                # else:
+                #     plot.title("spike train for input '1' over 1000 ms")
+                # plot.show()
+                # input("enter")
+
+                self.optimize_supervised(features, targets)  ### weight update 
             
-                for i in range(encodedData.shape[0]):
-                    features = encodedData[i,:-1,:]
-                    targets = encodedData[i,-1:,:]
-        #                 self.forward(features)
-                    self.optimize_supervised(features, targets)  ### weight update 
-                
-        
-        
-                #data_loader.get_n_batch(0)
-                #rawData = data_loader.get_batch(encoding = "raw") #column1: x, column2: y, column3: label
-        
-                #encodedData = data_loader.get_batch(encoding = "firing_rate")
-                preds = np.zeros((rawData.shape[0],1)) #(4,1)
-                rates = np.zeros((rawData.shape[0],1)) #(4,1)
-        
-                for i in range(rawData.shape[0]):
-        
-                    outSpikes = self.forward(encodedData[i,:-1,:])
-                    rates[i] = time_to_rate(outSpikes)
-                    preds[i] = time_to_bool(outSpikes)
+    
+    
+            #data_loader.get_n_batch(0)
+            #rawData = data_loader.get_batch(encoding = "raw") #column1: x, column2: y, column3: label
+    
+            #encodedData = data_loader.get_batch(encoding = "firing_rate")
+            preds = np.zeros((rawData.shape[0],1)) #(4,1)
+            rates = np.zeros((rawData.shape[0],1)) #(4,1)
+    
+            for i in range(rawData.shape[0]):
+    
+                outSpikes = self.forward(encodedData[i,:-1,:])
+
+                rates[i] = time_to_rate(outSpikes)
+                preds[i] = time_to_bool(outSpikes)
         
             error = np.sqrt(np.sum((preds - rawData[:,-1]) ** 2))
             if epoch % 10 == 0:
                 print("epoch: " + str(epoch))
                 print("\tWeights: " + str([self.layers[i].synapses for i in range(len(self.layers))]))
+                print("\traw inputs: " + str(rawData[:,:-1]))
                 print("\tTarget boolean value: " + str(rawData[:,-1]))
                 print("\tTarget firing rate: " + str([time_to_rate(encodedData[i,-1,:]) for i in range(encodedData.shape[0])]))
                 print("\tPredicted firing rate: " + str(rates.reshape(-1)))
@@ -133,6 +149,7 @@ class SNN_MLP(object):
         outputs = np.zeros((features.shape[0], self.layers[-1].synapses.shape[-1], features.shape[-1]))
         for i in range(features.shape[0]):
             outputs[i,:,:] = self.forward(features[i,:,:])
+            print("\tWeights: " + str([self.layers[i].synapses for i in range(len(self.layers))]))
         return outputs
 
 
