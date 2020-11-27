5403da95aaeb31781febdff705c34c03fdc4fd03
jiesparklin <33667998+jiesparklin@users.noreply.github.com>
2020-11-27 04:19:12 -0500

    Update snn_neuron.py

--- Assignment 2/Jie/snn_neuron.py
+++ Assignment 2/Jie/snn_neuron.py
@@ -2,7 +2,7 @@ import numpy as np
 from hyperparams import *
 class LIFFCLayer(object):
     def __init__(self, n_input, n_neuron, \
-                delta_t = 1.0, resistance = 10.0, conductance = 2.0, refact_time = 2,\
+                delta_t = 1.0, resistance = 10.0, conductance = 2.0,refact_time = 2.0,\
                 v_threshold = 0.75, v_resting = 0.0, spike_magnitude = 1.0, input_decay = 0.7):
         """
         @input
@@ -19,8 +19,10 @@ class LIFFCLayer(object):
         super(LIFFCLayer, self).__init__()
         # weights, or spike input efficacies; normalized so that sum of square of single neuron weights is 1
         W = np.random.uniform(0.0, 1.0, (n_input, n_neuron)) / n_input
-#         scale = np.sqrt(np.sum(W ** 2, axis = 0))
-#         self.synapses = W / scale
+
+ 
+        scale = np.sqrt(np.sum(W ** 2, axis = 0))
+        self.synapses = W / scale
         self.synapses = W
 
         # Simulation config (may not all be needed!!)
@@ -28,15 +30,14 @@ class LIFFCLayer(object):
         self.dt = delta_t
         # Current neuron potential (mV)
         self.Vm = np.zeros(n_neuron)
-        # Resting potential is set to 0
-        # self.Vrest = np.zeros(n_neuron)
+        # Refectory period
+        self.tau_ref  = refact_time
         # Resistance
         self.Rm = resistance
         # Conductance
         self.Cm = conductance
         # refactory period, originally 2
         self.tRef = refact_time
-        # refactory count down
         self.refCtDwn = np.zeros(n_neuron)
         # spike threshold, originally 0.75/1
         self.Vth = v_threshold
@@ -48,6 +49,9 @@ class LIFFCLayer(object):
         self.inVec = np.zeros(n_neuron)
         # input decay
         self.inDecay = input_decay
+ 
+
+
 
     def forward(self, in_train):
         """
@@ -65,14 +69,18 @@ class LIFFCLayer(object):
             outTrain[:,i] = spikes
         return outTrain, vHistory
     
-    def forward_step(self, features):
+    def forward_step(self, features,check):
         """
         forward step of a single time step
+        
         """
-        # new input spikes are mapped to through synapses
-        inputVec = np.matmul(features, self.synapses)
-        # previous currents leave traces
-        inputVec += self.inVec * self.inDecay
+
+        inputVec = np.matmul(features,self.synapses)
+
+
+        # else:
+        #     inputVec = features[0]*self.synapses[0]+features[1]*self.synapses[1]
+
         self.inVec = inputVec
         # leaky integrate and fire
         return self.do_LIF(inputVec)
@@ -80,9 +88,10 @@ class LIFFCLayer(object):
     def do_LIF(self, I):
         """
         One step of LIF update
-        """
-#         print("neuron potential: " + str(self.Vm))
-        # neurons that just fired
+        """         
+        
+ #        print("neuron potential: " + str(self.Vm))
+#        neurons that just fired
         firedNeurons = self.Vm > self.Vth
         # newly fired neurons will go back to resting potental and start refactory period
         self.Vm[firedNeurons] = self.Vr
@@ -94,13 +103,8 @@ class LIFFCLayer(object):
         # update neurons that is not in refactory period
         self.Vm = self.Vm + diff * (1-alreadyFiredNeurons) * self.dt
         self.refCtDwn[alreadyFiredNeurons] -= 1
-        # debug
-#         print("input: " + str(I))
-#         print("potential update: " + str(diff))
-#         print("updated neuron potential: " + str(self.Vm))
-#         print("fired neurons: " + str(firedNeurons))
-#         print("count down: " + str(self.refCtDwn))
-#         input()
+
+
         # return the newly fired neurons
         return firedNeurons * self.Vspike, self.Vm
 
